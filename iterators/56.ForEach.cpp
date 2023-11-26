f82732d457b3352cd6501f047d42a8204c356e57
Meheer <meherr17.j@gmail.com>
2023-11-26 17:32:36 +0530

    Timing Function

--- iterators/56.ForEach.cpp
+++ iterators/56.ForEach.cpp
@@ -10,15 +10,11 @@ void doubleNumber(int& i)
 int main()
 {
     std::array arr{ 1, 2, 3, 4 };
-
     std::for_each(arr.begin(), arr.end(), doubleNumber);
-
     for (int i : arr)
     {
         std::cout << i << ' ';
     }
-
     std::cout << '\n';
-
     return 0;
 }
